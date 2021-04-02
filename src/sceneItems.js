36e92cb4da6ace2a8136817374d2d6b7e7dd5f24
= <hodaman2012@gmail.com>
2021-04-02 21:17:31 +0200

    missing up lights for just fucken shadow

--- src/sceneItems.js
+++ src/sceneItems.js
@@ -4,6 +4,8 @@ import { loadModel } from "./ModelLoader";
 import {extract} from "./UI"
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 
+// import { ProgressiveLightMap } from 'three/examples/jsm/misc/ProgressiveLightMap';
+import { ProgressiveLightMap } from 'three/examples/jsm/misc/ProgressiveLightMap';
 // const hat = require("./Example_all feature.glb").default;
 // const earth = require("./Avatar3.glb").default;
 const earth = require("./model/GLB/Avatar (ALL)_GLB.glb").default;
@@ -11,21 +13,25 @@ const earth = require("./model/GLB/Avatar (ALL)_GLB.glb").default;
 const hdrbg = require("./model/Texture/courtyard_2k.hdr")
 var face, hair, cloth
 // console.log("HFFHFHHF",hdrbg.default)
+let lightmapObjects = [] ,dirLights = [] ,progressiveSurfacemap;
+let shadowMapRes = 512, lightMapRes = 1024, lightCount = 8;
 function addLights() {
   const amplight = new THREE.AmbientLight("#ffffff", 0.1);
-  let lightBack = new THREE.SpotLight(0xffffff, 1.4);
-  let lightFront = new THREE.SpotLight(0xffffff, 0.5);
+  let lightBack = new THREE.SpotLight(0xffffff, 1.4 ,2,0.1);
+  let lightFront = new THREE.SpotLight(0xffffff, 0.8,undefined,2);
   let hemiLight = new THREE.HemisphereLight(0xffeeb1,0x080820,2)
     hemiLight.position.set(0,1,10)
 
-    lightFront.position.set(1.1, 3, 3.7);
+    lightFront.position.set(2, 0, 4.5);
     lightBack.position.set(-10, 9,6);
   
   // lightBack.castShadow = true
   lightFront.castShadow = true
 
-
   // Backlight:
+  const params = { 'Enable': true, 'Blur Edges': true, 'Blend Window': 200,
+							 'Light Radius': 50, 'Ambient Weight': 0.5, 'Debug Lightmap': false };
+		
 
   const tl = new THREE.PointLight(0xffffff,0.4)
   const tr = new THREE.PointLight(0xffffff,0.4)
@@ -34,7 +40,7 @@ function addLights() {
   const xd = 5
   const zd = 2
   const yd = 5
-  const rightShift = 1
+  const rightShift = 2
   tl.position.set(xd+rightShift,yd,-zd)
   tr.position.set(-xd+rightShift,yd,-zd)
   bl.position.set(xd+rightShift,-yd,-zd)
@@ -53,6 +59,7 @@ scene.add(br)
 // scene.add(new THREE.PointLightHelper(bl))
 // scene.add(new THREE.PointLightHelper(br))
 
+// scene.add(new THREE.SpotLightHelper(lightFront,0xff0000))
   const rectLight = new THREE.RectAreaLight( 0xffffff, 1,  10, 10 );
   rectLight.position.set( 2, 0, 5 );
   rectLight.lookAt( 0, 0, 0 );
@@ -76,7 +83,7 @@ lightFront.shadow.camera.near = 0.1; // default
 lightFront.shadow.camera.far = 500; // default
 lightFront.shadow.focus = 1; // default
 // lightFront.shadow.radius =10
-lightFront.shadow.normalBias =0.08
+// lightFront.shadow.normalBias =0.08
 
 
  
@@ -107,7 +114,30 @@ setLighting()
   // scene.add(amplight);
   // scene.add(lightBack);
   scene.add(lightFront);
-  scene.add( rectLight ) 
+
+
+  progressiveSurfacemap = new ProgressiveLightMap( renderer, lightMapRes );
+
+  const lightCount=8
+  for ( let l = 0; l < lightCount; l ++ ) {
+
+    let dirLight = new THREE.DirectionalLight( 0xffffff, 1.0 / lightCount );
+    dirLight.name = 'Dir. Light ' + l;
+    dirLight.position.set( 2, 2, 2 );
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.near = 100;
+    dirLight.shadow.camera.far = 5000;
+    dirLight.shadow.camera.right = 150;
+    dirLight.shadow.camera.left = - 150;
+    dirLight.shadow.camera.top = 150;
+    dirLight.shadow.camera.bottom = - 150;
+    dirLight.shadow.mapSize.width = 1024;
+    dirLight.shadow.mapSize.height = 1024;
+    lightmapObjects.push( dirLight );
+    dirLights.push( dirLight );
+    
+  }
+  // scene.add( rectLight ) 
 
   // scene.add( new THREE.SpotLightHelper(lightBack,"#ff00cc") );
   // scene.add( new THREE.SpotLightHelper( lightFront ,"#ccff00"));
@@ -135,7 +165,7 @@ const addItem = () => {
       // //  cloth.castShadow = true; 
       //  cloth.receiveShadow = true;
 
-        face.castShadow = false; 
+        // face.castShadow = true; 
        face.receiveShadow = true;
       // //  face.material.wireframe = true
 
@@ -194,7 +224,30 @@ const addItem = () => {
       // const hair = extractMesh("hair")
       // hair.visible = false
       console.log("hair",hair)
+console.log("KIKI",e)
+      e.scene.traverse( function ( child ) {
 
+        if ( child.isMesh ) {
+
+          child.name = "Loaded Mesh";
+          // child.castShadow = true;
+          // child.receiveShadow = true;
+          // child.material = new THREE.MeshPhongMaterial();
+
+          // This adds the model to the lightmap
+          lightmapObjects.push( child );
+          progressiveSurfacemap.addObjectsToLightMap( lightmapObjects );
+
+        } else {
+
+          child.layers.disableAll(); // Disable Rendering for this
+
+        }
+
+      } );
+
+      // lightmapObjects.push( e.scene );
+      // progressiveSurfacemap.addObjectsToLightMap( lightmapObjects );
       scene.add(e.scene);
       render()
     })
